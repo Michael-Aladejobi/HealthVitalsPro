360d6630318a2c8dc293533fb772b3cc619548c2
___techbros <94129364+Michael-Aladejobi@users.noreply.github.com>
2024-06-11 17:14:15 +0100

    Update index.js
    Fixed minor bugs

--- index.js
+++ index.js
@@ -56,6 +56,16 @@ document
                 }
             );
         }
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage);
+            });
     });
 
 //FORGET PASSWORD - Event listener for "Forget Password" link
