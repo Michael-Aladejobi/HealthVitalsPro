d450f76bfcdaff9865fcbfa45ad3337fa40b2d56
___techbros <94129364+Michael-Aladejobi@users.noreply.github.com>
2024-06-27 13:44:36 +0100

    Update profileTest.js

--- profileTest.js
+++ profileTest.js
@@ -91,23 +91,25 @@ function addTableRow(dataTableBody, patientID, readingID, reading) {
     //if (!(reading.temperature === 36.5 && reading.temperature === 37.2)) {
       //  temperatureCell.style.backgroundColor = "#c71f2d";
     //}
-
+ if ((reading.temperature>0) {
+       temperatureCell.style.backgroundColor = "grey";
+    }
     //Available for upgrades
 
-    // // Check conditions for pulse
-    // if (reading.pulse < 60 || reading.pulse > 100) {
-    //     pulseCell.style.backgroundColor = "#c71f2d";
-    // }
+    // Check conditions for pulse
+    if (reading.pulse > 0) {
+        pulseCell.style.backgroundColor = "red";
+    }
 
-    // // Check conditions for height
-    // if (reading.height < 150) {
-    //     heightCell.style.backgroundColor = "#c71f2d";
-    // }
+    // Check conditions for height
+    if (reading.height > 0) {
+        heightCell.style.backgroundColor = "green";
+    }
 
-    // // Check conditions for weight
-    // if (reading.weight > 100) {
-    //     weightCell.style.backgroundColor = "#c71f2d";
-    // }
+    // Check conditions for weight
+    if (reading.weight > 0) {
+        weightCell.style.backgroundColor = "#blue";
+    }
 }
 
 // function to save data with your own patient IDs
