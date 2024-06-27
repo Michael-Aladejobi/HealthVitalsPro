fc0f146558b2d77009937831e42ab619bb2ab0b2
___techbros <94129364+Michael-Aladejobi@users.noreply.github.com>
2024-06-27 12:39:15 +0100

    Update ESP32_code.ino

--- ESP32 _code_to_send_data_to_firebase.ino
+++ ESP32 _code_to_send_data_to_firebase.ino
@@ -3,10 +3,23 @@
 #include <Firebase_ESP_Client.h>
 #include <WebServer.h>
 #include "time.h"
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+#include "MAX30100_PulseOximeter.h"
+#include <LiquidCrystal_I2C.h>
+
+
+// Calibration constants from the linear regression
+const float slope = 38.30;
+const float intercept = 0.00;
+
+// Define the pin for the analog input
+const int analogPin = 34; // GPIO pin connected to the voltage output
+
 
 // Insert your network credentials
-#define WIFI_SSID "Ettronics Hightech"
-#define WIFI_PASSWORD "11111111"
+#define WIFI_SSID "Ettronics"
+#define WIFI_PASSWORD "00000000"
 
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyBAgeUvIPwMirPSPywX9YZJZ79htSpKO5c"
@@ -19,14 +32,22 @@
 #define DATABASE_URL "https://wrud-5b418-default-rtdb.firebaseio.com/"
 
 // Define HC-SR04 sensor pins
-const int trigPin1 = 5;
-const int echoPin1 = 18;
-const int trigPin2 = 22;
-const int echoPin2 = 23;
-const int trigPin3 = 19;
-const int echoPin3 = 21;
-const int trigPin4 = 4;
-const int echoPin4 = 2;
+const int trigPin = 5;
+const int echoPin = 18;
+
+// Define button pins
+const int heightButtonPin = 19;
+const int temperatureButtonPin = 4;
+const int uploadResultPin = 15;
+const int pulseButtonPin = 13;
+const int weightButtonPin = 27;
+
+// Debounce variables
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;  // 50 milliseconds debounce delay
+
+// Create a temperature sensor object
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 // Define Firebase objects
 FirebaseData fbdo;
@@ -36,14 +57,21 @@ FirebaseConfig config;
 // Variable to save USER UID
 String uid;
 
+// Variable to save the distance between U-sensor and the footstool
+const float HEIGHT_STAND = 215.50;
+
+// Offset variable to calibrate temperature sensor
+const float TEMPERATURE_OFFSET_VALUE = 4;
+
 // Database main path (to be updated in setup with the user UID)
 String databasePath;
 // Database child nodes
-String temperaturePath = "temperature"; //trigPin1 | echoPin1
-String pulsePath = "pulse"; //trigPin2 | echoPin2
 String heightPath = "height";
-String weightPath = "weight";
+String tempPath = "temperature";
+String pulsePath = "pulse";
+String spo2Path = "spo2";
 String timePath = "timestamp";
+String weightPath = "weight";
 
 // Parent Node (to be updated in every loop)
 String parentPath;
@@ -51,28 +79,31 @@ String parentPath;
 unsigned long timestamp;
 FirebaseJson json;
 
+// Pulse oximeter variables
+#define REPORTING_PERIOD_MS 1000
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+
+void onBeatDetected() {
+    Serial.println("Beat!");
+}
+
 // Timer variables (send new readings every three minutes) 
-//OVERIDE by Reset Button
-unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 180000; // 3 minutes
+// Override by Reset Button
+//unsigned long sendDataPrevMillis = 0;
+//unsigned long timerDelay = 180000; // 3 minutes
 
 WebServer server(80);
 String customUserID;
 
-//Initialize WiFi
-void initWiFi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println();
-  Serial.print("Connected to WiFi with IP: ");
-  Serial.println(WiFi.localIP());
-}
+// Variables to store sensor measurements
+float heightMeasurement = 0.0;
+float temperatureMeasurement = 0.0;
+float pulseMeasurement = 0.0;
+float spo2Measurement = 0.0;
+float weightMeasurement = 0.0;
 
-//Track Current Epoch Time
+// Track Current Epoch Time
 unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
@@ -80,7 +111,7 @@ unsigned long getTime() {
     return 0;
   }
   time(&now);
-  return now + 3600; //3600 seconds(1 hour) to my epoch time
+  return now + 3600; // 3600 seconds (1 hour) to my epoch time
 }
 
 // Function to get local time as a string, GMT+0
@@ -104,24 +135,40 @@ String getFormattedTime() {
   return String(timeString);
 }
 
-// Function to get distance from HC-SR04 sensor
-float getDistance(int trigPin, int echoPin) {
-  // Clear the trigPin
+// Function to get height from HC-SR04 sensor
+float getHeight() {
+  // Send a 10us pulse to trigger the sensor
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-
-  // Trigger the sensor
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Read the echoPin
+  // Measure the echo time in microseconds
   long duration = pulseIn(echoPin, HIGH);
 
-  // Calculate the distance
-  float distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-
+  // Calculate the distance in cm
+  float distance = duration * 0.034 / 2.0;
+  
   return distance;
+}
+
+// Function to get temperature from MLX90614 sensor
+float getTemperature() {
+  return mlx.readObjectTempC(); // You can choose to use readAmbientTempC() if needed
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println();
+  Serial.print("Connected to WiFi with IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 // CREATE WEBSERVER HOST --init--
@@ -199,7 +246,6 @@ void handleSubmit() {
 
 void updateFirebaseData() {
   if (Firebase.ready()) {
-
     // Get current timestamp
     timestamp = getTime();
     Serial.print("time: ");
@@ -209,49 +255,53 @@ void updateFirebaseData() {
     String formattedTime = getFormattedTime();
     Serial.print("Formatted time: ");
     Serial.println(formattedTime);
-
-    // Get distance from HC-SR04 sensors
-    float temperature = getDistance(trigPin1, echoPin1);
-    float pulse = getDistance(trigPin2, echoPin2);
-    float height = getDistance(trigPin3, echoPin3);
-    float weight = getDistance(trigPin4, echoPin4);
-
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-    Serial.print("Pulse: ");
-    Serial.println(pulse);
-    Serial.print("Height: ");
-    Serial.println(height);
-    Serial.print("Weight: ");
-    Serial.println(weight);
-
+    
     if (customUserID.length() > 0) {
       parentPath = "/UsersData/" + customUserID + "/readings/" + String(timestamp);
 
-      json.set(temperaturePath, String(temperature));
-      json.set(pulsePath, String(pulse));
-      json.set(heightPath, String(height));
-      json.set(weightPath, String(weight));
+      json.set(heightPath, String(heightMeasurement));
+      json.set(tempPath, String(temperatureMeasurement));
+      json.set(pulsePath, String(pulseMeasurement));
+      json.set(spo2Path, String(spo2Measurement));
+      json.set(weightPath, String(weightMeasurement));
+
       json.set(timePath, formattedTime);
       Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+     
     } else {
-      Serial.println("No custom user ID provided.");
+      Serial.println("Enter Patient ID on Webpage.");
     }
   }
 }
 
+LiquidCrystal_I2C lcd(0x27, 16, 2); //LCD code 
+
+
 void setup() {
+  
   Serial.begin(115200);
 
+
+  lcd.init();                    //LCD code 
+  lcd.backlight();               //LCD code   
+
+  lcd.setCursor(0,0);            //LCD code 
+  lcd.print("Initializing...");         //LCD code 
+
   // Initialize HC-SR04 sensor pins
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(trigPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
-  pinMode(trigPin4, OUTPUT);
-  pinMode(echoPin4, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // Initialize buttons
+  pinMode(heightButtonPin, INPUT_PULLUP);
+  pinMode(temperatureButtonPin, INPUT_PULLUP);
+  pinMode(uploadResultPin, INPUT_PULLUP);
+  pinMode(pulseButtonPin, INPUT_PULLUP);
+  pinMode(weightButtonPin, INPUT_PULLUP);
+
+
+  // Initialize temperature sensor
+  mlx.begin();
 
   initWiFi();
   configTime(0, 0, "pool.ntp.org");
@@ -269,35 +319,127 @@ void setup() {
   Firebase.reconnectWiFi(true);
   fbdo.setResponseSize(4096);
 
-  // Initialize the library with the Firebase authen and config
   Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while (auth.token.uid == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
-
-  // Update database path
-  databasePath = "/UsersData/" + uid + "/readings";
-
-  // Initialize web server
-  server.on("/", HTTP_GET, handleRoot);
+  // Web server
+  server.on("/", handleRoot);
   server.on("/submit", HTTP_POST, handleSubmit);
   server.begin();
+
+  // Initialize the pulse sensor
+  if (!pox.begin()) {
+    Serial.println("FAILED TO INITIALIZE PULSE OXIMETER");
+    for (;;);
+  } else {
+    Serial.println("Pulse oximeter initialized");
+    lcd.setCursor(0,0); 
+     lcd.print("                ");
+
+    lcd.setCursor(0,0);            //LCD code 
+  lcd.print("Test Result:");         //LCD code 
+  }
+  pox.setOnBeatDetectedCallback(onBeatDetected);
 }
 
 void loop() {
-  server.handleClient();
-
-  // Check if it's time to send new readings to Firebase
-  if (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0) {
-    sendDataPrevMillis = millis();
-    updateFirebaseData();
+  server.handleClient();  
+  
+  // Update pulse oximeter readings
+  pox.update();
+  
+  // Read height measurement on button press with debounce
+  if (digitalRead(heightButtonPin) == LOW) {
+    delay(50); // Debounce delay
+    if (digitalRead(heightButtonPin) == LOW) {
+      heightMeasurement = HEIGHT_STAND - getHeight();
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+       lcd.setCursor(0, 1);
+    lcd.print("Height: " + String(heightMeasurement) + "cm"); 
+      Serial.print("Height: ");
+      Serial.print(heightMeasurement);
+      Serial.println("cm");
+        delay(500);
+    }
   }
+
+  // Read temperature measurement on button press with debounce
+  if (digitalRead(temperatureButtonPin) == LOW) {
+    delay(50); // Debounce delay
+    if (digitalRead(temperatureButtonPin) == LOW) {
+      temperatureMeasurement = getTemperature() + TEMPERATURE_OFFSET_VALUE;
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+           lcd.setCursor(0, 1);
+           lcd.print("Temp: " + String(temperatureMeasurement) + " *c");
+      Serial.print("Temperature: ");
+      Serial.print(temperatureMeasurement);
+      Serial.println(" *C");
+        delay(500);
+    }
+  }
+  
+  // Read pulse and oxygen measurement on button press with debounce
+  if (digitalRead(pulseButtonPin) == LOW) {
+    delay(50); // Debounce delay
+    if (digitalRead(pulseButtonPin) == LOW) {
+      pulseMeasurement = pox.getHeartRate();
+      spo2Measurement = pox.getSpO2();
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print("Pulse: " + String(pulseMeasurement) + " BPM");
+      Serial.print("Pulse: ");
+      Serial.print(pulseMeasurement);
+      Serial.print(" BPM");
+      Serial.print(", SpO2: ");
+      Serial.print(spo2Measurement);
+      Serial.println(" %");
+      
+    }
+      
+  }
+
+
+if (digitalRead(weightButtonPin) == LOW) {
+    delay(50); // Debounce delay
+    if (digitalRead(weightButtonPin) == LOW) {
+      
+       
+       // Read the analog input (voltage)
+  int adcValue = analogRead(analogPin);
+  
+  // Convert the ADC value to voltage (assuming a 12-bit ADC with 3.3V reference)
+  float voltage = adcValue * (3.3 / 4095.0);
+  
+  float weightMeasurement = (voltage * slope) + intercept;
+  lcd.setCursor(0, 1);
+      lcd.print("                ");
+       lcd.setCursor(0, 1);
+    lcd.print("Weight: " + String(weightMeasurement) + " kg"); 
+      Serial.print("Weight: ");
+      Serial.print(weightMeasurement);
+      Serial.println(" kg");
+        delay(500);
+    }
+}
+
+
+  
+  // Upload result on button press with debounce
+  if (digitalRead(uploadResultPin) == LOW)
+  {
+    delay(50); // Debounce delay
+    if (digitalRead(uploadResultPin) == LOW) 
+    {
+      updateFirebaseData();
+        delay(500);
+        lcd.setCursor(0, 1);
+      lcd.print("                ");
+         lcd.setCursor(0, 1);
+      lcd.print("Data Uploaded");
+    }
+  }
+  
 }
